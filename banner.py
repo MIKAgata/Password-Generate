bffb640321af970eb064020c8a17603a3d54d94e
MIKE <mikael.agata.sitepu@gmail.com>
2026-03-15 12:13:47 +0700

    add banner.txt, and modified banner.py

--- banner.py
+++ banner.py
@@ -1,1 +1,12 @@
-print("hello word")
+def show_banner():
+    with open("banner.txt", "r", encoding="utf-8") as file:
+        banner = file.read()
+        print(banner)
+
+
+def main():
+    show_banner()
+
+
+if __name__ == "__main__":
+    main()
