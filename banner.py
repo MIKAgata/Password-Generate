0044e5b2a2b2c48f3cd2d7999c77b067d49c8c8e
MIKE <mikael.agata.sitepu@gmail.com>
2026-03-16 12:54:48 +0700

    modified banner.py

--- banner.py
+++ banner.py
@@ -1,3 +1,12 @@
+from colorama import Fore, Style, init
+
+init()
+
+def show_banner():
+    with open("banner.txt", "r", encoding="utf-8") as f:
+        banner = f.read()
+
+    print(Fore.GREEN + banner + Style.RESET_ALL)
 def show_banner():
     with open("banner.txt", "r", encoding="utf-8") as file:
         banner = file.read()
